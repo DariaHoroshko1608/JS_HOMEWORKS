d9df2d261bee034beb36842f052c161f22b78cf5
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-02-24 19:28:51 +0200

    hw_7

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 'use strict'
+
+//Task #1
+
+const name = 'John';
+const admin = name;
+alert(admin);
+
+//Task #2
+
+// 1
+
+const planet = 'Venera';
+console.log(planet);
+
+// 2
+
+const userName = "User Name";
+const nominal = prompt(userName);
+alert(nominal);
+console.log(nominal);
+
+//Task #3
+
+const ten = '10' + 10;
+console.log(ten);
