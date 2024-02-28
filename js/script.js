90b343a8f2e6acaacc8b6eca215515f249abb9c4
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-02-28 21:15:01 +0200

    hw__9

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 'use strict'
 
-let temperature = parseFloat(prompt ('Введіть температуру'));
-let unit = prompt('Введіть одиницю виміру (C чи F):');
+const temperature = parseFloat(prompt ('Введіть температуру'));
+const unit = prompt('Введіть одиницю виміру (C чи F):');
 if (unit.toUpperCase()==="C") {
-    let convertedTemperature = (temperature * 9/5) + 32;
+    const convertedTemperature = (temperature * 9/5) + 32;
     alert('Температура у градусах Фаренгейта: ' + convertedTemperature.toFixed(2) + 'F');
 }
 else if (unit.toUpperCase()==='F'){
-    let convertedTemperature = (temperature - 32) * 5/9;
+    const convertedTemperature = (temperature - 32) * 5/9;
     alert('Температура у градусах Цельсія: ' + convertedTemperature.toFixed(2) + 'C');
 }
