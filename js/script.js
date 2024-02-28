4c4127b63d8dfdb664ae41037b8ff9601192146d
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-02-28 15:21:45 +0200

    hw__6

--- js/script.js
+++ js/script.js
@@ -2,23 +2,23 @@
 
 // Частина 1
 
-let lastName =prompt( 'Last name');
-let firstName = prompt('First name');
-let age = prompt( 'Age');
-let userFull = lastName + ' ' + firstName + ' ' + age;
+const lastName =prompt( 'Last name');
+const firstName = prompt('First name');
+const age = prompt( 'Age');
+const userFull = lastName + ' ' + firstName + ' ' + age;
 
 alert(userFull);
 console.log(userFull);
 
 // Частина 2
 
-let number = 12345;
+const number = 12345;
 
-let five = number % 10;
-let four = Math.floor(number / 10) % 10;
-let three = Math.floor(number / 100) % 10;
-let two = Math.floor(number / 1000) % 10;
-let one = Math.floor(number / 10000) % 10;
+const five = number % 10;
+const four = Math.floor(number / 10) % 10;
+const three = Math.floor(number / 100) % 10;
+const two = Math.floor(number / 1000) % 10;
+const one = Math.floor(number / 10000) % 10;
 
 console.log(one, two, three, four, five);
 
