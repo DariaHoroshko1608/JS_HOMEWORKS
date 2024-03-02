d9f9f571bb4273ad76e4b5984762556a00134139
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-02 13:09:29 +0200

    hw_10

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 'use strict'
+
+const operation = prompt('Що ви хочете зробити?((add, sub, mult, div))');
+const numOne = parseFloat(prompt('Введіть перше число'));
+const numTwo = parseFloat(prompt('Введіть друге число'));
+let result;
+
+
+switch (operation){
+    case 'add':
+        result = numOne + numTwo;
+        alert(`${numOne} + ${numTwo} = ${result}`);
+        break;
+    case  'sub':
+        result = numOne - numTwo;
+        alert(`${numOne} - ${numTwo} = ${result}`);
+        break;
+    case 'mult':
+        result = numOne * numTwo;
+        alert(`${numOne} * ${numTwo} = ${result}`);
+        break;
+    case 'div':
+        if (numTwo !==0){
+            result = numOne / numTwo;
+            alert(`${numOne} / ${numTwo} = ${result}`);
+        }
+        else {
+            alert(`Ділення на 0 неможливе`);
+        }
+        break;
+    default:
+        alert('Некоректне введення');
+}
