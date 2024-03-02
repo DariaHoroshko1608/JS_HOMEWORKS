849bf3dc706dcaf49e98a17c0bf62c2c59fd6c49
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-02 14:02:05 +0200

    hw_11

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict'
+
+const numOne = parseFloat(prompt('Запишіть число'));
+const numTwo = parseFloat(prompt('Запишіть число'));
+
+const addResult = numOne + numTwo;
+console.log(`Результат: ${numOne} + ${numTwo} = ${addResult}`);
+
+const subResult = numOne - numTwo;
+console.log(`Результат: ${numOne} - ${numTwo} = ${subResult}`);
+
+const multResult = numOne * numTwo;
+console.log(`Результат: ${numOne} * ${numTwo} = ${multResult}`);
+
+
+if (numTwo !== 0) {
+    const divResult = numOne / numTwo;
+    console.log(`Результат: ${numOne} / ${numTwo} = ${divResult}`);
+} else {
+    console.log("Ділення на нуль неможливе!");
+}
+
+
