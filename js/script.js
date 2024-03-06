3d706b2466a24f8d40019c1911ba418b8821f865
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-06 14:54:25 +0200

    hw_15

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const operation = prompt("Що ви хочете зробити? (add, sub, mult, div)").toLowerCase();
+
+const numOne = parseFloat(prompt("Введіть перше число:"));
+const numTwo = parseFloat(prompt("Введіть друге число:"));
+
+let result = null;
+if (operation === 'add') {
+    result = numOne + numTwo;
+} else if (operation === 'sub') {
+    result = numOne - numTwo;
+} else if (operation === 'mult') {
+    result = numOne * numTwo;
+} else if (operation === 'div') {
+    if (numTwo !== 0) {
+        result = numOne / numTwo;
+    } else {
+        alert("Ділення на нуль!");
+        throw new Error("Ділення на нуль!");
+    }
+}
+
+alert(numOne + " " + operation + " " + numTwo + " = " + result);
