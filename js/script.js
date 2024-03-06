7e844e9b15786c2c310a3d4259c6c828f74ceaea
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-06 14:17:53 +0200

    hw_16

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 'use strict'
+
+const numOne = parseFloat(prompt('Щоб отримати середнє арифметичне трьох числел введіть перше число...'));
+const numTwo = parseFloat(prompt('Введіть друге число'));
+const numThree = parseFloat(prompt('Введіть друге число'));
+let average = (numOne + numTwo + numThree)/3;
+
+alert('Середнє арифметичне ваших чисел ' + average);
