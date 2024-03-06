0cf49806e5aa19ababccc4bb150a9269be757948
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-06 14:32:45 +0200

    hw_14

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict'
+
+const numOne = parseFloat(prompt('Введіть перше число'));
+const numTwo = parseFloat(prompt('Введіть друге число'));
+
+const add = numOne + numTwo;
+const mult = numOne * numTwo;
+const div = numOne / numTwo;
+const sub = numOne - numTwo;
+
+alert (numOne + " + " + numTwo + " = " + add + "\n" +
+    numOne + " - " + numTwo + " = " + sub + "\n" +
+    numOne + " * " + numTwo + " = " + mult + "\n" +
+    numOne + " / " + numTwo + " = " + div);
