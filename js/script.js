bdc116a30ec5f1037d4191ba4bf3cbe5ccf9ec0e
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-10 15:22:08 +0200

    hw_19

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const userNumber = prompt("Введіть число:");
+
+if (!isNaN(userNumber) && userNumber !== null) {
+    let number = parseInt(userNumber);
+    let suffix = null;
+
+    if (number === 1) {
+        suffix = "рік";
+    } else if (number >= 2 && number <= 4) {
+        suffix = "роки";
+    } else {
+        suffix = "років";
+    }
+
+    alert(number + " " + suffix);
+
+} else {
+    alert("Будь ласка, введіть коректне число!");
+}
