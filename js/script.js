87770bd3128e05877c5895a7b2865d10164b0cd8
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-10 14:39:28 +0200

    hw_18

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 'use strict'
+
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
+switch (true) {
+    case numOrStr === null:
+        console.log('Ви скасували');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+    case isNaN(+numOrStr):
+        console.log('number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+        break;
+}
+
+// switch (numOrStr) {
+//     case null :
+//         console.log('Ви скасували');
+//         break;
+//     case '' :
+//         console.log('Empty String');
+//         break;
+//     case isNaN(+numOrStr):
+//         console.log('number is Ba_NaN'); * не працює(
+//         break;
+//     default:
+//         console.log('OK!');
+//         break;
+// }
