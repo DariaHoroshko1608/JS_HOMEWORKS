f84c629aa6ed3525d18e85df3ef39d76c27e50b8
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-14 16:14:11 +0200

    home_25

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict'
+
+const number = parseInt(prompt("Введіть ціле число:"));
+
+if (!isNaN(number)) {
+    if (number <= 0) {
+        console.log("NaN");
+    }
+    let divider = 2;
+    while (number % divider !== 0) {
+        divider++;
+    }
+    console.log(`Мінімальний дільник числа: ${divider}`);
+} else {
+    console.log("NaN");
+}
