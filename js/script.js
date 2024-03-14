4fc67fffa2c12b6a06e5ed99a44dfed924065b1a
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-14 15:28:10 +0200

    home_24

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict'
+
+//1
+// const arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++){
+//     sum = sum + arr[i]
+// }
+// console.log("Сума елементів масиву:", sum);
+
+// 2
+
+const arr = [1, 2, 3, 4, 5];
+let sumNumber = 0;
+for (let i = 0; i < arr.length; i++){
+    sumNumber += arr[i] * arr[i];
+}
+console.log("Сума квадратів елементів масиву:", sumNumber);
