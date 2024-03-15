5409892535e05aae651bf4a831c4694019f78bd4
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-15 15:17:34 +0200

    home_28

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const userNumber = function (number = 0, degree = 1){
+    if (typeof number !== 'number' || typeof degree !== 'number'){
+        return 'Помилка, всі значення мають бути числами';
+    }
+    return Math.pow(number , degree);
+}
+const num = prompt('Введіть число');
+const deg = prompt('Введіть ступінь');
+const result = userNumber(Number(num), Number(deg));
+alert(`Результат піднесення числа ${num} до ступеню ${deg} дорівнює ${result}`);
