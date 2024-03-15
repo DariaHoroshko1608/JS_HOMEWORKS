e56fd1f2fd569429c9e05a6a0dfb2bd96aa65be2
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-15 14:52:40 +0200

    home_27

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const generateKey = function (length, charsets) {
+    let key = '';
+    for (let i = 0; i < length; i++){
+        key += charsets [Math.floor(Math.random() * charsets.length)]
+    }
+    return key;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);
