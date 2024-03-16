915d28303a64b9dbb3f4d75c203185bfd1ed2f85
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-16 18:21:14 +0200

    home_29

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const padString = function (str, len,padChar = ' ',padLeft = false){
+const padString = function (str, len,padChar = '*',padLeft = true){
     if (typeof str !== 'string') return 'Помилка! Має бути рядок!';
     if (typeof len !== "number" || len < 0) return 'Помилка! Має бути додатнє число!';
     if (typeof padChar !== 'string' || padChar.length !==1 ) return 'Помилка! Має бути рядок довжиною один символ';
@@ -9,10 +9,10 @@ const padString = function (str, len,padChar = ' ',padLeft = false){
     if (str.length >= len) return str.substr(0, len);
 
     const padding = Array(len - str.length + 1).join(padChar);
-    return padLeft ? padChar + str + padding : padding + str;
+    return padLeft ? str + padding : padding + str;
 }
 
-console.log(padString('hello', 8, '*', true));
+console.log(padString('hello', 8));
 console.log(padString('hello', 6, '*', false));
 console.log(padString('hello', 2));
 console.log(padString(1608, 4));
