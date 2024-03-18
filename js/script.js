e94a6decaec7a4b76ad15c6d51786375a0a105fc
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-18 13:26:11 +0200

    home_30

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
 'use strict'
 
-const  getPositiveNumbers= function (arr){
-    if (!Array.isArray(arr) || arr.length === 0) {
-        return 'Invalid input data';
+const arr = [1, 2, 3, -1, -2, -3];
+const filterPositiveNumbers = function (array){
+    const isArrayEmpty = (array) => {
+        return array.length === 0;
+    };
+
+    if (isArrayEmpty(array)) {
+        return "Порожній масив";
     }
 
     const positiveNumbers = [];
 
-    for (let i = 0; i < arr.length; i++) {
-        const currentNumber = arr[i];
+    for (let i = 0; i < array.length; i++) {
+        const currentNumber = array[i];
 
         if (currentNumber > 0) {
             positiveNumbers.push(currentNumber);
         }
     }
 
-    return positiveNumbers.length > 0 ? positiveNumbers : null;
+    if (positiveNumbers.length > 0) {
+        return positiveNumbers;
+    }
+    return null;
 };
 
-const arr = [1, 2, 3, -1, -2, -3];
-console.log(getPositiveNumbers(arr)); // [1, 2, 3]
-
-const emptyArr = [];
-console.log(getPositiveNumbers(emptyArr)); // null
-
-const invalidData = 'Invalid data';
-console.log(getPositiveNumbers(invalidData)); // 'Invalid input data'
+console.log(filterPositiveNumbers(arr));
