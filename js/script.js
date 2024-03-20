2be63ada6265e2ab21d8bc6b0a2ba078b1cb2739
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-20 21:46:12 +0200

    home_30_3

--- js/script.js
+++ js/script.js
@@ -1,29 +1,47 @@
 'use strict'
 
-const arr = [1, 2, 3, -1, -2, -3];
-const filterPositiveNumbers = function (array){
-    const isArrayEmpty = (array) => {
-        return array.length === 0;
-    };
-
-    if (isArrayEmpty(array)) {
-        return "Порожній масив";
-    }
-
+// const arr = [1, 2, 3, -1, -2, -3];
+// const filterPositiveNumbers = function (array){
+//     const isArrayEmpty = (array) => {
+//         return array.length === 0;
+//     };
+//
+//     if (isArrayEmpty(array)) {
+//         return "Порожній масив";
+//     }
+//
+//     const positiveNumbers = [];
+//
+//     for (let i = 0; i < array.length; i++) {
+//         const currentNumber = array[i];
+//
+//         if (currentNumber > 0) {
+//             positiveNumbers.push(currentNumber);
+//         }
+//     }
+//
+//     if (positiveNumbers.length > 0) {
+//         return positiveNumbers;
+//     }
+//     return null;
+// };
+//
+// console.log(filterPositiveNumbers(arr));
+function filterPositiveNumbers(arr) {
     const positiveNumbers = [];
 
-    for (let i = 0; i < array.length; i++) {
-        const currentNumber = array[i];
+    if (arr.length === 0) {
+        return "Масив порожній";
+    }
 
-        if (currentNumber > 0) {
-            positiveNumbers.push(currentNumber);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0 && positiveNumbers.push(arr[i])) {
         }
     }
 
-    if (positiveNumbers.length > 0) {
-        return positiveNumbers;
-    }
-    return null;
-};
+    return positiveNumbers.length > 0 ? positiveNumbers : null;
+}
 
-console.log(filterPositiveNumbers(arr));
+// Приклад використання:
+const arr = [1, 2, 3, -1, -2, -3];
+console.log(filterPositiveNumbers(arr)); // Виведе: [1, 2, 3]
