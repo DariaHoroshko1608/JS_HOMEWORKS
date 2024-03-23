f44931eedc72c260f3bb1842f34fc8c0a34e91c0
Dasha Horoshko <schekachovadasha1608@gmail.com>
2024-03-23 19:54:20 +0200

    home_30.4

--- js/script.js
+++ js/script.js
@@ -1,47 +1,15 @@
 'use strict'
 
-// const arr = [1, 2, 3, -1, -2, -3];
-// const filterPositiveNumbers = function (array){
-//     const isArrayEmpty = (array) => {
-//         return array.length === 0;
-//     };
-//
-//     if (isArrayEmpty(array)) {
-//         return "Порожній масив";
-//     }
-//
-//     const positiveNumbers = [];
-//
-//     for (let i = 0; i < array.length; i++) {
-//         const currentNumber = array[i];
-//
-//         if (currentNumber > 0) {
-//             positiveNumbers.push(currentNumber);
-//         }
-//     }
-//
-//     if (positiveNumbers.length > 0) {
-//         return positiveNumbers;
-//     }
-//     return null;
-// };
-//
-// console.log(filterPositiveNumbers(arr));
-function filterPositiveNumbers(arr) {
-    const positiveNumbers = [];
+const arr = [1, 2, 3, -1, -2, -3];
 
-    if (arr.length === 0) {
-        return "Масив порожній";
+const foo = function (array) {
+    const result = [];
+    if (!array.length) return 'Порожній масив';
+    for (let i = 0; i < array.length; i++) {
+        const isPositive = Math.sign(array[i]) === 1 || Math.sign(array[i]) === 0
+        isPositive && result.push(array[i])
     }
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > 0 && positiveNumbers.push(arr[i])) {
-        }
-    }
-
-    return positiveNumbers.length > 0 ? positiveNumbers : null;
+    return result.length ? result : null;
 }
 
-// Приклад використання:
-const arr = [1, 2, 3, -1, -2, -3];
-console.log(filterPositiveNumbers(arr)); // Виведе: [1, 2, 3]
+console.log(foo(arr));
