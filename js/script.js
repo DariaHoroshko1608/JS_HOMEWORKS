5873a406738ee13358adf0d0bf8c837b5b87486e
DariaHoroshko1608 <schekachovadasha1608@gmail.com>
2024-03-24 19:14:19 +0200

    home_33,1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+// Константа сумування
+const add = (a, b) => {
+    return a + b;
+};
+
+
+const subtract = (a, b) => {
+    return a - b;
+};
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+const result1 = add(5, 3);
+const result2 = subtract(10, 4);
+const result3 = multiply(2, 7);
