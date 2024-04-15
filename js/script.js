eb03e7618ffad39065f781b313454d080278b367
DariaHoroshko1608 <schekachovadasha1608@gmail.com>
2024-04-15 16:21:53 +0300

    home_52_2

--- js/script.js
+++ js/script.js
@@ -1,42 +1,36 @@
-"use strict";
-
-//1
+// 1
 const getFactorial = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
 
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-
-    return n * getFactorial(n - 1);
+  return n * getFactorial(n - 1);
 };
 
 const number = 3;
 console.log(`Факторіал числа ${number} дорівнює: ${getFactorial(number)}`);
 
-//2
+// 2
 const pow = (num, degree) => {
+  if (degree === 0) {
+    return 1;
+  }
 
-    if (degree === 0) {
-        return 1;
-    }
-
-    return num * pow(num, degree - 1);
+  return num * pow(num, degree - 1);
 };
-
 
 const num = 10;
 const degree = 2;
 console.log(`${num} в степені ${degree} дорівнює: ${pow(num, degree)}`);
 
-//3
+// 3
 
 const recursiveSum = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
 
-    if (b === 0) {
-        return a;
-    }
-
-    return recursiveSum(a + 1, b - 1);
+  return recursiveSum(a + 1, b - 1);
 };
 
 const a = 8;
