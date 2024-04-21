c66cb854bec983c2e50b933c72489e618e9e31d6
DariaHoroshko1608 <schekachovadasha1608@gmail.com>
2024-04-21 12:07:36 +0300

    home_54

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-"use strict";
+const customBind = (func, context) => {
+  if (typeof func !== 'function') {
+    throw new TypeError('Error, none function');
+  }
+
+  const bindArgs = Array.prototype.slice.call(arguments, 2);
+
+  return function () {
+    const args = Array.prototype.slice.call(arguments);
+
+    const combinedArgs = bindArgs.concat(args);
+
+    return func.apply(context, combinedArgs);
+  };
+};
+
+const obj = {
+  name: 'Vova :) ',
+};
+
+function greet(message) {
+  console.log(`${message}, ${this.name}`);
+}
+
+const boundFunc = customBind(greet, obj, 'Hello');
+boundFunc();
