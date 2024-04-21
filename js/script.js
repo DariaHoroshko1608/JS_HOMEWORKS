2dfe40eac28eeb107e03ee209d8bf9123baf149c
DariaHoroshko1608 <schekachovadasha1608@gmail.com>
2024-04-21 16:11:27 +0300

    home_56

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-"use strict";
+const myApply = (func, context, argsArray) => {
+  if (typeof func !== 'function') {
+    throw new TypeError('Error, none function');
+  }
+
+  context = context || window;
+
+  const methodName = `__myApply_method_${Math.floor(Math.random() * 1000000)}`;
+
+  context[methodName] = func;
+
+  const args = argsArray.map((arg, index) => `argsArray[${index}]`).join(',');
+
+  const functionCallCode = `context.${methodName}(${args})`;
+
+  const result = eval(functionCallCode);
+
+  delete context[methodName];
+
+  return result;
+};
+
+const greet = (message) => {
+  console.log(`${message}, ${context.name}`);
+};
+
+const context = {
+  name: 'Vova :) ',
+};
+
+myApply(greet, context, ['Hello']);
