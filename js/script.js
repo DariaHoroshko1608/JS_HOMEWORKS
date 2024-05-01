7fd7f2e00ccdfdbb14dddd9cf35b2233e34d3bf5
DariaHoroshko1608 <schekachovadasha1608@gmail.com>
2024-05-01 14:29:28 +0300

    home_57

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-"use strict";
+(function () {
+  const sum = (param) => {
+    let result = param;
+    return (number) => {
+      result += number;
+      return result;
+    };
+  };
+  const exemple = sum(11);
+  console.log(exemple(21));
+  alert(exemple(8));
+}());
