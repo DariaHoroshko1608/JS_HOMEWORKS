78a5e127b4e8e53ba75c400bf2e6309ad59d6e94
DariaHoroshko1608 <schekachovadasha1608@gmail.com>
2024-05-16 14:48:21 +0300

    hw_62

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const generatedHtmlList = (function generateList(array) {
+  let html = "<ul>";
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      html += "<li>" + generateList(item) + "</li>";
+    } else {
+      html += "<li>" + item + "</li>";
+    }
+  });
+  html += "</ul>";
+  return html;
+})([1, 2, 3]);
+
+console.log(generatedHtmlList);
