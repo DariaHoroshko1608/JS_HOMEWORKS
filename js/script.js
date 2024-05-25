8d43c8154e75b5423729ab597cd7ed88852b4a99
DariaHoroshko1608 <schekachovadasha1608@gmail.com>
2024-05-25 15:22:46 +0300

    hw_66

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
-"use strict";
+(function () {
+  document.addEventListener("DOMContentLoaded", () => {
+    const itemList = document.getElementById("itemList");
+
+    function saveFavorites(favorites) {
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+    }
+
+    function toggleFavorite(itemId) {
+      const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+      const index = favorites.indexOf(itemId);
+      if (index === -1) {
+        favorites.push(itemId);
+      } else {
+        favorites.splice(index, 1);
+      }
+      saveFavorites(favorites);
+      renderItems();
+    }
+
+    function renderItems() {
+      itemList.innerHTML = "";
+
+      for (let i = 1; i <= 5; i++) {
+        const li = document.createElement("li");
+        li.textContent = `Елемент ${i}`;
+        const favoriteBtn = document.createElement("button");
+        favoriteBtn.textContent = JSON.parse(
+          localStorage.getItem("favorites"),
+        )?.includes(i)
+          ? "Видалити"
+          : "Додати";
+        favoriteBtn.classList.add("favorite-btn");
+        favoriteBtn.addEventListener("click", () => {
+          toggleFavorite(i);
+        });
+        li.appendChild(favoriteBtn);
+        itemList.appendChild(li);
+      }
+    }
+
+    renderItems();
+  });
+})();
