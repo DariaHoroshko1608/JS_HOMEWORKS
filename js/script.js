7a92217fb0fed093625fe1d1d771521e2ae03314
DariaHoroshko1608 <schekachovadasha1608@gmail.com>
2024-06-11 14:17:49 +0300

    hw_68

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-"use strict";
+(function () {
+  function createArrayIterator(arr) {
+    let index = 0;
+
+    return {
+      next() {
+        if (index < arr.length) {
+          return { value: arr[index++], done: false };
+        }
+        return { value: undefined, done: true };
+      },
+    };
+  }
+
+  Array.prototype[Symbol.iterator] = function () {
+    return createArrayIterator(this);
+  };
+
+  const arr = [1, 2, 3, 4, 5];
+  const iterator = arr[Symbol.iterator]();
+
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
+})();
