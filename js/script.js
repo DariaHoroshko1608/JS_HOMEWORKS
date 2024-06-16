36299314c8851377e3e5a4c3739b0743ae65895a
DariaHoroshko1608 <schekachovadasha1608@gmail.com>
2024-06-16 16:18:50 +0300

    hw_69

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
-"use strict";
+const cachedExampleFunction = (function () {
+  function cacheFunction(func) {
+    const cache = new Map();
+
+    return function () {
+      const args = JSON.stringify(arguments);
+
+      if (cache.has(args)) {
+        console.log(`Cache hit for ${args}`);
+        return cache.get(args);
+      }
+
+      const result = func.apply(this, arguments);
+      cache.set(args, result);
+
+      if (cache.size > 10) {
+        const firstKey = cache.keys().next().value;
+        console.log(`Removing first cache entry: ${firstKey}`);
+        cache.delete(firstKey);
+      }
+
+      console.log("Current cache size:", cache.size);
+      return result;
+    };
+  }
+
+  function exampleFunction(a, b) {
+    console.log(`Computing ${a} ${b}`);
+    return a + b;
+  }
+
+  return cacheFunction(exampleFunction);
+})();
+
+console.log(cachedExampleFunction(1, 2));
+console.log(cachedExampleFunction(1, 2));
+console.log(cachedExampleFunction(2, 3));
+console.log(cachedExampleFunction(2, 3));
+
+for (let i = 0; i < 12; i++) {
+  console.log(`Result for ${i}, ${i + 1}: ${cachedExampleFunction(i, i + 1)}`);
+}
